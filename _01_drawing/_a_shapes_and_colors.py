c5b2c73ed4c3625c4588f93017c3de3bf3f5ccfc
root <nk33233@pacificcoastacademy.org>
2023-01-19 02:23:22 +0000

    change pentagon.

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,25 +5,34 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    turtle_ = turtle.Turtle()
+
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    turtle_.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    turtle_.speed(10)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    turtle_.color('blue')
+    turtle_.pencolor('green')
+    turtle_.goto(0,0)
     # Move your turtle forward using .forward(100)
+    for i in range(100):
+        turtle_.forward(100)
     # TEST    Did your turtle move forward?
 
     # Move your turtle left or right using .left(90) or .right(90)
-
+        turtle_.left(2)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
+    #yes
 
     # Move your turtle to a new place on the screen using .goto(x, y)
-    # x=0 and y=0 is the center of the screen
+        turtle_.goto(0,0)
 
+    # x=0 and y=0 is the center of the screen
+        turtle_.goto(0,-10)
+        turtle_.circle(50, steps=30)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
 
--- _02_code_flow/_b_pentagon_crazy.py
+++ _02_code_flow/_b_pentagon_crazy.py
@@ -21,30 +21,33 @@ if __name__ == '__main__':
     colors = ('red', 'blue', 'green', 'yellow', 'orange')
     
     # Make a new turtle
-    
+    turtle_ = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    turtle_.shape('circle')
     # Set the turtle speed to max (0)
-    
+    turtle_.speed(0)
     # Set the turtle width to 1
-    
+    turtle_.width(1)
     # Create a variable to hold the number of sides in a pentagon
-    
+    sides = 5
     # Create a variable to be the angle of 360 divided by the sides variable
-    
+    angle = 360/sides
     # Use a for loop to repeat ALL the following lines of code 360 times. 
-        
+    for i in range(460):
         # If the loop variable (i) is equal to 100, set the turtle width to 2
-        
+        if i == 100:
+            turtle_.width(2)
         # If the loop variable (i) is equal to 200, set the turtle width to 3
-        
+        if i == 200:
+            turtle_.width(3)
         # Use the get_next_color function to set the turtle pencolor,
+        turtle_.pencolor(get_next_color(i))
         # *hint .pencolor(get_next_color(i))
         
         # Move the turtle forward by the loop variable, *hint .forward(i)
-        
+        turtle_.forward(i)
         # Turn the turtle to the right by the angle variable + 1
-
+        turtle_.right(angle + 1)
     # Hide your turtle so you can see the pattern.
         
     # Check the pattern against the picture in the recipe. If it matches, you are done!
